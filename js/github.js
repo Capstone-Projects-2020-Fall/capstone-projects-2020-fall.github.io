d0b445a5442afe35f33e3c7766a3bf037f08fc17
Ian Tyler Applebaum <Ian.tyler@electricbluefish.com>
2020-11-12 21:25:22 -0500

    Publish deploy 2020-11-12 21:25

--- js/github.js
+++ js/github.js
@@ -1,17 +1,6 @@
 // import GitHub from 'github-api';
 // basic auth
-var gh = new GitHub({
-   username: 'Applebaumian',
-   /* also acceptable:
-	*/
-	  token: '9607486a3499c89e08c4c881e09b745a88141efb'
-});
 
-function getTeamMembers(org, repo){
-	let repository = gh.getRepo(org,repo);
-	let team = repository.getContributorStats();
-	return team;
-}
 // var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
 // me.listNotifications(function(err, notifications) {
 //    // do some stuff
